71ed87fb463dd990628aae2b600c1ee930be9c18
retr0lbb <85702153+retr0lbb@users.noreply.github.com>
2022-08-05 15:06:57 -0300

    Add files via upload

---
+++ Funcionario.java
@@ -0,0 +1,11 @@
+
+public class Funcionario extends PessoaFisica {
+    private String cartao;
+    
+    public void setCartao(String cartao){
+        this.cartao = cartao;
+    }
+    public String getCartao(){
+        return(cartao);
+    } 
+}
---
+++ PessoaFisica.java
@@ -0,0 +1,11 @@
+
+public class PessoaFisica extends Pessoa{
+    private String rg;
+    
+    public void setRg(String rg){
+        this.rg = rg;
+    }
+    public String getRg(){
+        return(rg);
+    }
+}
